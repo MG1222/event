acf1d846cdb38d30402de6f8041dd1739fff088b
Margad Gantuya <majagantuya@gmail.com>
2019-11-04 16:21:32 +0100

    make it beeettteer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,5 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(name: params[:name], email: params[:email])
-    if @user.save
-      puts "ok "
-    else 
-      logger.debug "User created:@user.inspect"
-      puts "#{@user}"
-      puts "#{@user.errors.inspect}"
-  end
-      
+	end   
 end
