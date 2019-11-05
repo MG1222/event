c432809f61e8715120fea0fe947290f220c51c64
Margad Gantuya <majagantuya@gmail.com>
2019-11-05 09:44:48 +0100

    make it beeettteer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,9 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-# 
-  config.action_mailer.delivery_method = :letter_opener
+# LETTER_OPENER
+
+  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
 
