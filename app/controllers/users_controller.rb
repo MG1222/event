d2f812f52a76a9fd7ee82f13aa3f82f84b686490
Margad Gantuya <majagantuya@gmail.com>
2019-11-06 00:25:44 +0100

    make it beeettteer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
+# include UsersHelper
+  
+ # before_action :authenticate_user! #, except: [:index]
+ # before_action :match_current_user #, except: [:index]
 
-  def create
-    @user = User.new(name: params[:name], email: params[:email])
-	end   
-end
+  # def index
+  #   redirect_to root_path  
+  # end
+
+    def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(first_name: params[:first_name], last_name: params[:last_name], description: params[:description])
+    redirect_to @user
+  end
+ 
+end # class
